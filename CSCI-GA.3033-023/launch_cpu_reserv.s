2ba8fbe05402b967708e56d90cd14b629b4890e1
srinidhigoud <srinidhi.goud29@gmail.com>
2018-03-12 18:25:48 -0400

    run

--- CSCI-GA.3033-023/launch_cpu_reserv.s
+++ CSCI-GA.3033-023/launch_cpu_reserv.s
@@ -21,5 +21,23 @@
 
 #Uncomment to execute pytorch code
 module load pytorch/python3.6/0.3.0_4
+python ./lab1.pytorch 4
+python ./lab1.pytorch 5
+python ./lab1.pytorch 6
+python ./lab1.pytorch 7
+python ./lab1.pytorch 8
+python ./lab1.pytorch 9
+python ./lab1.pytorch 10
+python ./lab1.pytorch 11
+python ./lab1.pytorch 12
+python ./lab1.pytorch 13
+python ./lab1.pytorch 14
 python ./lab1.pytorch 15
+python ./lab1.pytorch 16
+python ./lab1.pytorch 17
+python ./lab1.pytorch 18
+python ./lab1.pytorch 19
+python ./lab1.pytorch 20
+python ./lab1.pytorch 21
+python ./lab1.pytorch 22
 
