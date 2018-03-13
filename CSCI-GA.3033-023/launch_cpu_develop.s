79403ac3bf09a380e49ebde5f491133f91d44785
srinidhigoud <sgm400@nyu.edu>
2018-03-12 21:21:13 -0400

    run

--- CSCI-GA.3033-023/launch_cpu_develop.s
+++ CSCI-GA.3033-023/launch_cpu_develop.s
@@ -20,16 +20,4 @@
 
 #Uncomment to execute pytorch code
 module load pytorch/python3.6/0.3.0_4
-python ./lab1_1.pytorch 13
-python ./lab1_1.pytorch 14
-python ./lab1_1.pytorch 15
-python ./lab1_1.pytorch 16
-python ./lab1_1.pytorch 17
-python ./lab1_1.pytorch 18
-python ./lab1_1.pytorch 19
-python ./lab1_1.pytorch 20
-python ./lab1_1.pytorch 21
-python ./lab1_1.pytorch 22
-python ./lab1_1.pytorch 23
-python ./lab1_1.pytorch 24
-python ./lab1_1.pytorch 25
+python ./lab1.pytorch
